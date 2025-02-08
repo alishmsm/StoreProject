a14481d1b81c2de189fca95fae3d16c0a251bd40
alishmsm <alimotlagh45@gmail.com>
2025-02-08 21:33:46 +0330

    add Architecture

---
+++ Site.EndPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Site.EndPoint.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
